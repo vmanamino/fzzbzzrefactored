af0a7e43ee961015b14f68c9cccb74e47acc1a51
vmanamino <vmanamino@gmail.com>
2016-03-22 03:57:41 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//   for (var count = 1; count <= 100; count++) {
+//     //   $('.target').append(count + "<br/>");
+//     if ( count % 3 == 0 && count % 5 == 0 ) {
+//         $('.target').append("fizz buzz<br/>");
+//     }
+//     else if ( count % 3 == 0) {
+//         $('.target').append("fizz<br/>");   
+//     }
+//     else if ( count % 5 == 0) {
+//         $('.target').append("buzz<br/>");
+//     }
+//     else {
+//         $('.target').append(count + "<br/>");
+//     }
+//   }
+// });
+$(document).ready(function() {
+   var input = prompt("Please enter a number: ")
+});
