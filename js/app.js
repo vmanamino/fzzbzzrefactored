642dfc352c8f76a24375c72f5a77b71107842c33
vmanamino <vmanamino@gmail.com>
2016-03-22 04:36:45 +0000

    basic functionality

--- js/app.js
+++ js/app.js
@@ -16,5 +16,24 @@
 //   }
 // });
 $(document).ready(function() {
-   var input = prompt("Please enter a number: ")
+   var input = prompt("Please enter a number: ");
+   if (parseInt(input)) {
+       for (var count = 1; count <= input; count++) {
+           if ( count % 3 == 0 && count % 5 == 0 ) {
+               $('.target').append("fizz buzz<br/>");
+            }
+            else if ( count % 3 == 0) {
+               $('.target').append("fizz<br/>");   
+            }
+            else if ( count % 5 == 0) {
+               $('.target').append("buzz<br/>");
+            }
+            else {
+               $('.target').append(count + "<br/>");
+            }
+        }
+   }
+   else {
+       $('.target').append("Your input, " + input + ", is not valid <br/>");
+   }
 });
