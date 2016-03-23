ca89fea4b650f0dfdb57dd2a713500bc8ff82072
vmanamino <vmanamino@gmail.com>
2016-03-23 00:45:52 +0000

    added page input

--- js/app.js
+++ js/app.js
@@ -16,24 +16,29 @@
 //   }
 // });
 $(document).ready(function() {
-   var input = prompt("Please enter a number: ");
-   if (parseInt(input)) {
-       for (var count = 1; count <= input; count++) {
-           if ( count % 3 == 0 && count % 5 == 0 ) {
-               $('.target').append("fizz buzz<br/>");
-            }
-            else if ( count % 3 == 0) {
-               $('.target').append("fizz<br/>");   
-            }
-            else if ( count % 5 == 0) {
-               $('.target').append("buzz<br/>");
-            }
-            else {
-               $('.target').append(count + "<br/>");
-            }
-        }
-   }
-   else {
-       $('.target').append("Your input, " + input + ", is not valid <br/>");
-   }
+   $("#target").submit(function(event){
+      event.preventDefault();
+      var input = $('.target input').val();
+      input  +input;
+      $('.target input').val("");
+      if (parseInt(input)) {
+          for (var count = 1; count <= input; count++) {
+              if ( count % 3 == 0 && count % 5 == 0 ) {
+                  $('.target').append("fizz buzz<br/>");
+               }
+               else if ( count % 3 == 0) {
+                  $('.target').append("fizz<br/>");   
+               }
+               else if ( count % 5 == 0) {
+                  $('.target').append("buzz<br/>");
+               }
+               else {
+                  $('.target').append(count + "<br/>");
+               }
+          }
+      }
+      else {
+         $('.target').append("Your input, " + input + ", is not valid <br/>");
+      }
+   });
 });
